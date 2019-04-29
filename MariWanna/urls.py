221d50324c4834494f8fc9d5415aaec17eb0b098
Frederick Kauber <fdkauber@gmail.com>
2019-04-29 02:20:51 -0400

    First Part of Login

--- MariWanna/urls.py
+++ MariWanna/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import url
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 from . import views
 
 urlpatterns = [
     path('', views.home),
     # url('search', views.search),
+    url('login', views.login),
+    url('perform-signin', views.perform_signin),
+    url('create-account', views.create_account),
+    url('enter-user', views.enter_user),
     url('search-similar', views.similar_search),
     url('search-custom', views.custom_search),
     url('results-similar', views.similar_results),
