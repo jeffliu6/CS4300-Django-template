39641237ea607dc182bfc153bcda59ccfb5ca30f
Noel Picinich <np@tsl.io>
2019-04-29 03:12:14 -0400

    strain results and modal style for similar search

--- MariWanna/static/js/common.js
+++ MariWanna/static/js/common.js
@@ -1,19 +1,5 @@
 $(document).ready(function(){
-    var height = $(window).height();
-
-    let nav_height = $("nav").height();
     
-    $("#mainContent").css("min-height", height-nav_height);
-
-    $('#loader')
-    // .hide()  // Hide it initially
-    .ajaxStart(function() {
-        $(this).show();
-    })
-    .ajaxStop(function() {
-        $(this).hide();
-    })
-;
 
     $(window).resize( function() {
         $("#mainContent").css("min-height", $(window).height() - nav_height);
--- MariWanna/static/js/search_similar.js
+++ MariWanna/static/js/search_similar.js
@@ -15,15 +15,14 @@ $(document).ready(function() {
         .done(function( data ) {
             data = JSON.parse(data);
             
-            $('#results').empty();
+            $('#similar-results').empty();
             
             let count = 0;
 
             data.forEach(function(strain){
 
                 console.log(strain);
-                
-                $("#results").append('<div id="strain_'+ count +'" class="card strain-result ml-2 mr-2 mb-2 shadow">' + 
+                $("#similar-results").append('<div id="strain_'+ count +'" class="card strain-result ml-2 mr-2 mb-2 shadow">' + 
                 // '<img src="' + strain[1]["image"] +'" class="card-img-top" alt="...">' +
                 '<div class="card-body">' +
                     '<div class="d-flex justify-content-between"><h5 class="card-title font-weight-bolder mb-1">' + strain[1]["name"] +'</h5><p class="text-muted text-small">' + strain[0].toFixed(2) + '</p></div>' +
@@ -34,24 +33,55 @@ $(document).ready(function() {
                 '</div>');
                 
                 $("#strain_" + count).on("click", function() {
+
+                    $("#modal-img").empty();
+                    if (strain[1]["image"] != "https://www.cannabisreports.com/images/strains/no_image.png") {
+                        $("#modal-img").append('<img style="max-height:300px" src="' + strain[1]["image"] +'" class="rounded mb-3 img-fluid" alt="...">');
+                    }
                     $("#modal-name").text(strain[1]["name"]);
                     $("#modal-description").text(strain[1]["description"]);
-                    $("#modal-medical").text(strain[1]["medical"]);
-                    $("#modal-desired").text(strain[1]["positive"]);
-                    $("#modal-undesired").text(strain[1]["negative"]);                    
-                    $("#modal-flavors").text(strain[1]["flavors"]);
-                    $("#modal-aromas").text(strain[1]["aromas"]);
+
+                    if (strain[1]["medical"]) {
+                        $("#modal-medical").text(strain[1]["medical"].join(", "));
+                    } else {
+                        $("#modal-medical-label").hide();
+                    }
+                    
+                    if (strain[1]["positive"]) {
+                        $("#modal-desired").text(strain[1]["positive"].join(", "));
+                    } else {
+                        $("#modal-desired-label").hide();
+                    }
+
+                    if (strain[1]["negative"]) {
+                        console.log(strain[1]["negative"].length)
+                        $("#modal-undesired").text(strain[1]["negative"].join(", "));   
+                    } else {
+                        $("#modal-undesired-label").hide();
+                    }
+
+                    if (strain[1]["flavor_descriptors"]) {
+                        $("#modal-flavors").text(strain[1]["flavor_descriptors"].join(", "));
+                    } else {
+                        $("#modal-flavors-label").remove();
+                    }      
+                    
+                    if (strain[1]["aroma"]) {
+                        $("#modal-aromas").text(strain[1]["aroma"].join(", "));
+                    } else {
+                        $("#modal-aromas-label").remove();
+                    }                
+
+                    
                 });
 
                 count++;
-
             });
-
             $("#loadingDiv").fadeOut();
+
             $("#similarSearch").slideUp();
-            console.log(requestData.strain);
-            $("#results-header").text("Strains similar to " + requestData.strain + ":").show();
-            $("#results").addClass("d-flex").show(500);
+            $("#results-header").html("Strains similar to <b>" + requestData.strain + "</b>:").show();
+            $("#similar-results").addClass("d-flex").show(500);
 
 
         });
